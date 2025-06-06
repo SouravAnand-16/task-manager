6e198867a6962a5597b21f4e080faf0ba071b47d
Sourav Anand <sauravanand324@gmail.com>
2025-06-06 21:55:26 +0530

    fix:routes handler

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// const { createTask, getTasks, bulkUpdateTasks } = require('../controllers/taskController');
-// const auth = require('../middleware/authMiddleware');
+const { createTask, getTasks, bulkUpdateTasks } = require('../controllers/taskController');
+const auth = require('../middleware/authMiddleware');
 
-// router.get('/', auth, getTasks);
-// router.post('/', auth, createTask);
-// router.patch('/bulk', auth, bulkUpdateTasks);
+router.get('/', auth, getTasks);
+router.post('/', auth, createTask);
+router.patch('/bulk', auth, bulkUpdateTasks);
 
 module.exports = router;
