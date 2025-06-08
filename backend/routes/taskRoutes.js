6878214916995a9f6350c35abd9ca72f23fc0ec2
Sourav Anand <sauravanand324@gmail.com>
2025-06-08 17:07:28 +0530

    fix: update task function

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks, bulkUpdateTasks } = require('../controllers/taskController');
+const { createTask, getTasks, bulkUpdateTasks, updateTask } = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
 
 router.get('/', auth, getTasks);
 router.post('/', auth, createTask);
+router.patch('/:taskId', auth, updateTask);
 router.patch('/bulk', auth, bulkUpdateTasks);
 
 module.exports = router;
