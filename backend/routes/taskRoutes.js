dbf5f0ee10f1d90c75c5c9cf11cb49966e743a3a
Sourav Anand <sauravanand324@gmail.com>
2025-06-08 14:15:48 +0530

    fix: task route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { createTask, getTasks, bulkUpdateTasks } = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
 
-router.get('/', auth, getTasks);
-router.post('/', auth, createTask);
+router.get('/me', auth, getTasks);
+router.post('/me', auth, createTask);
 router.patch('/bulk', auth, bulkUpdateTasks);
 
 module.exports = router;
